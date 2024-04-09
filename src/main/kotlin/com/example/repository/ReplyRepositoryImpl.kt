2ef45fbc39111436bce02c982bdbc7446369cbab
Edrees <mohamededrees234@hotmail.com>
2024-04-09 15:39:03 +0200

    fixed upvote and downvote replies

--- src/main/kotlin/com/example/repository/ReplyRepositoryImpl.kt
+++ src/main/kotlin/com/example/repository/ReplyRepositoryImpl.kt
@@ -1,20 +1,37 @@
 package com.example.repository
 
+import com.example.data.models.Post
 import com.example.data.models.Reply
-import com.example.data.models.Tag
+import com.example.data.models.now
 import com.example.data.requests.ReplyRequest
+import com.example.data.responses.PostResponse
 import com.example.data.responses.ReplyResponse
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import org.litote.kmongo.addToSet
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.eq
+import org.litote.kmongo.pull
+import org.litote.kmongo.setValue
 
 class ReplyRepositoryImpl(db: CoroutineDatabase) : ReplyRepository {
     private val replyCollection = db.getCollection<Reply>()
+    private val postCollection = db.getCollection<Post>()
 
     override suspend fun createReply(response: ReplyResponse): Boolean {
+        val post = postCollection.findOne(Post::id eq response.postId) ?: return false
+        postCollection.updateOne(
+            Post::id eq response.postId,
+            setValue(
+                Post::votes, post.replyCount + 1
+            )
+        )
         return replyCollection.insertOne(response.toEntity()).wasAcknowledged()
     }
 
     override suspend fun fetchReplies(request: ReplyRequest.FetchRequest): List<Reply> {
-        val replies = replyCollection.find().toList().filter{
+        val replies = replyCollection.find().toList().filter {
             it.postId == request.postId
         }
         return replies
@@ -31,23 +48,96 @@ class ReplyRepositoryImpl(db: CoroutineDatabase) : ReplyRepository {
     }
 
     override suspend fun upvoteReply(request: ReplyRequest.UpvoteRequest): Boolean {
-        val reply = replyCollection.findOneById(request.replyId)?: return false
+        if (request.userId.isBlank()) {
+            return false
+        }
+        val reply = replyCollection.findOne(Reply::id eq request.replyId)?: return false
         val userAlreadyVoted = reply.upvoted.contains(request.userId)
-        return if (userAlreadyVoted) {
-            false
+        if (userAlreadyVoted) {
+            replyCollection.updateOne(
+                Reply::id eq request.replyId,
+                pull(Reply::upvoted, request.userId)
+            )
+            replyCollection.updateOne(
+                Reply::id eq request.replyId,
+                setValue(
+                    Reply::votes, reply.votes - 1
+                )
+            )
+            return true
         } else {
-            replyCollection.updateOneById(reply.id, reply.copy(votes = reply.votes + 1, upvoted = reply.upvoted + request.userId)).wasAcknowledged()
+            if (reply.downvoted.contains(request.userId)) {
+                replyCollection.updateOne(
+                    Reply::id eq request.replyId,
+                    pull(Reply::downvoted, request.userId)
+                )
+                replyCollection.updateOne(
+                    Reply::id eq request.replyId,
+                    setValue(
+                        Reply::votes, reply.votes + 2
+                    )
+                )
+            }else {
+                replyCollection.updateOne(
+                    Reply::id eq request.replyId,
+                    setValue(
+                        Reply::votes, reply.votes + 1
+                    )
+                )
+            }
+            replyCollection.updateOne(
+                Reply::id eq request.replyId,
+                addToSet(Reply::upvoted, request.userId)
+            )
+            return true
         }
     }
 
     override suspend fun downvoteReply(request: ReplyRequest.DownvoteRequest): Boolean {
-        val reply = replyCollection.findOneById(request.replyId)?: return false
+        if (request.userId.isBlank()) {
+            return false
+        }
+        val reply = replyCollection.findOne(Reply::id eq request.replyId)?: return false
         val userAlreadyVoted = reply.downvoted.contains(request.userId)
-        return if (userAlreadyVoted) {
-            false
+        if (userAlreadyVoted) {
+            replyCollection.updateOne(
+                Reply::id eq request.replyId,
+                pull(Reply::downvoted, request.userId)
+            )
+            replyCollection.updateOne(
+                Reply::id eq request.replyId,
+                setValue(
+                    Reply::votes, reply.votes + 1
+                )
+            )
+            return true
         } else {
-            replyCollection.updateOneById(reply.id, reply.copy(votes = reply.votes - 1, upvoted = reply.downvoted + request.userId)).wasAcknowledged()
-        }    }
+            if (reply.upvoted.contains(request.userId)) {
+                replyCollection.updateOne(
+                    Reply::id eq request.replyId,
+                    pull(Reply::upvoted, request.userId)
+                )
+                replyCollection.updateOne(
+                    Reply::id eq request.replyId,
+                    setValue(
+                        Reply::votes, reply.votes - 2
+                    )
+                )
+            }else {
+                replyCollection.updateOne(
+                    Reply::id eq request.replyId,
+                    setValue(
+                        Reply::votes, reply.votes - 1
+                    )
+                )
+            }
+            replyCollection.updateOne(
+                Reply::id eq request.replyId,
+                addToSet(Reply::downvoted, request.userId)
+            )
+            return true
+        }
+    }
 
     override suspend fun reportReply(request: ReplyRequest.ReportRequest): Boolean {
         return true
